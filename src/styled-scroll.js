d791f31abc15811f24e0a62d4e35379636333138
Nick Largen <github@nicklargen.com>
2015-07-05 08:02:31 -0400

    Update scrollbar only when necessary instead of polling every frame
    The entire scrollbar will update if either the scrolling area or the
    scrolled content changes, but scrolls will only update the indicator.

--- src/styled-scroll.js
+++ src/styled-scroll.js
@@ -1,4 +1,6 @@
 (function (window, document, Math) {
+	'use strict';
+	
 	var requestAnimationFrame = window.requestAnimationFrame ||
 		window.webkitRequestAnimationFrame ||
 		window.mozRequestAnimationFrame ||
@@ -18,6 +20,76 @@
 	var startEvents = supportsPointer ? [pointerDown] : ['touchstart', 'mousedown'];
 	var moveEvents = supportsPointer ? [pointerMove] : ['touchmove', 'mousemove'];
 	var endEvents = supportsPointer ? [pointerUp, pointerCancel] : ['touchend', 'mouseup', 'touchcancel', 'mousecancel'];
+
+	//Source: http://www.backalleycoder.com/2013/03/18/cross-browser-event-based-element-resize-detection/
+	var attachEvent = document.attachEvent;
+	var isIE = navigator.userAgent.match(/Trident/);
+	
+	var requestFrame = (function () {
+		var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame ||
+			function (fn) { return window.setTimeout(fn, 20); };
+		return function (fn) { return raf(fn); };
+	})();
+
+	var cancelFrame = (function () {
+		var cancel = window.cancelAnimationFrame || window.mozCancelAnimationFrame || window.webkitCancelAnimationFrame ||
+			window.clearTimeout;
+		return function (id) { return cancel(id); };
+	})();
+
+	function resizeListener(e) {
+		var win = e.target || e.srcElement;
+		if (win.__resizeRAF__) cancelFrame(win.__resizeRAF__);
+		win.__resizeRAF__ = requestFrame(function () {
+			var trigger = win.__resizeTrigger__;
+			trigger.__resizeListeners__.forEach(function (fn) {
+				fn.call(trigger, e);
+			});
+		});
+	}
+
+	function objectLoad(e) {
+		this.contentDocument.defaultView.__resizeTrigger__ = this.__resizeElement__;
+		this.contentDocument.defaultView.addEventListener('resize', resizeListener);
+	}
+
+	window.addResizeListener = function (element, fn) {
+		if (!element.__resizeListeners__) {
+			element.__resizeListeners__ = [];
+			if (attachEvent) {
+				element.__resizeTrigger__ = element;
+				element.attachEvent('onresize', resizeListener);
+			}
+			else {
+				if (getComputedStyle(element).position == 'static') element.style.position = 'relative';
+				var obj = element.__resizeTrigger__ = document.createElement('object');
+				obj.setAttribute('style', 'display: block; position: absolute; top: 0; left: 0; height: 100%; width: 100%; overflow: hidden; pointer-events: none; z-index: -1;');
+				obj.__resizeElement__ = element;
+				obj.onload = objectLoad;
+				obj.type = 'text/html';
+				if (isIE) element.appendChild(obj);
+				obj.data = 'about:blank';
+				if (!isIE) element.appendChild(obj);
+			}
+		}
+		element.__resizeListeners__.push(fn);
+	};
+
+	window.removeResizeListener = function (element, fn) {
+		element.__resizeListeners__.splice(element.__resizeListeners__.indexOf(fn), 1);
+		if (!element.__resizeListeners__.length) {
+			if (attachEvent) element.detachEvent('onresize', resizeListener);
+			else {
+				element.__resizeTrigger__.contentDocument.defaultView.removeEventListener('resize', resizeListener);
+				element.__resizeTrigger__ = !element.removeChild(element.__resizeTrigger__);
+			}
+		}
+	};
+
+
+
+
+
 
 
 	var scrollbarWidth = null;
@@ -72,24 +144,53 @@
 			//Prevent user from scrolling the scrollbar into view
 			this.parent.style.overflow = 'hidden';
 		}
+		
+		// ----- SCROLLBAR HANDLING -----
+		this.initScrollbar();
 
-		this.init();
+		var self = this;
+		var requestScrollbarUpdate = function () { 
+			self.requestUpdate(); 
+			self.updateScrollbar = true;
+		};
+		
+		this.scrollElement.addEventListener('scroll', function () { self.requestUpdate(); });
+		
+		// create an observer instance
+		var observer = new MutationObserver(requestScrollbarUpdate);
+		var config = { attributes: true, childList: true, subtree: true };
+
+		observer.observe(this.scrollElement, config);
+
+		addResizeListener(this.scrollElement, requestScrollbarUpdate);
+		
+		requestScrollbarUpdate();
 	}
 
 	StyledScroll.prototype = {
 		
-		init: function () {
+		initScrollbar: function () {
 			var scrollbarDiv = createScrollbarElement('v', true, 'custom');
-
 			this.parent.appendChild(scrollbarDiv);
 			
-			var indicator = new Scrollbar(this, { el: scrollbarDiv });
-			this.scrollbar = indicator;
-			
-			(function animloop() {
-				requestAnimationFrame(animloop);
-				indicator.updateScrollbar();
-			})();
+			this.scrollbar = new Scrollbar(this, { el: scrollbarDiv });
+		},
+		
+		// Do not allow stacked update requests
+		requestUpdate: function () {
+			if (!this.isUpdateRequested) {
+				this.isUpdateRequested = true;
+				var self = this;
+				requestAnimationFrame(function () { self.update(); });
+			}
+		},
+		
+		update: function () {
+			if (this.updateScrollbar) {
+				this.scrollbar.updateScrollbar();
+				this.updateScrollbar = false;
+			} else this.scrollbar.updateIndicatorPosition();
+			this.isUpdateRequested = false;
 		},
 
 		scrollToElement: function () { },
@@ -121,9 +222,10 @@
 		this.indicator = this.wrapper.children[0];
 		this.indicatorStyle = this.indicator.style;
 		this.scrollElement = styledScroll.scrollElement;
+		this.styledScroll = styledScroll;
 
 		this.lastPointY = 0;
-		this.offset = 0;
+		this.indicatorDeltaY = 0;
 
 		var self = this;
 		startEvents.forEach(function (eventName) { 
@@ -137,33 +239,38 @@
 	
 	Scrollbar.prototype = {
 		updateScrollbar: function () {
-			if (this.scrollElement.clientHeight === this.scrollElement.scrollHeight) {
+			var scrollHeight = this.scrollElement.scrollHeight;
+			var clientHeight = this.scrollElement.clientHeight;
+			
+			if (clientHeight === scrollHeight) {
 				this.wrapperStyle.visibility = 'hidden';
 				return;
 			} else this.wrapperStyle.visibility = 'visible';
 			
-			if (this.offset != 0) {
-				this.scrollElement.scrollTop += this.offset / this.scrollbarToElementRatio();
-				this.offset = 0;
-			}
-
-			this.wrapperHeight = this.wrapper.clientHeight;
-			this.indicatorHeight = this.wrapperHeight * this.scrollElement.clientHeight / this.scrollElement.scrollHeight;
+			var wrapperHeight = this.wrapper.clientHeight;
+			
+			var indicatorHeight = wrapperHeight * clientHeight / scrollHeight;
 			//A quick benchmark showed Math.max performance to be worse than an if statement on IE11
-			if (this.indicatorHeight < 10) {
-				this.indicatorHeight = 10;
+			if (indicatorHeight < 10) {
+				indicatorHeight = 10;
 			}
-
-			this.indicator.style.height = this.indicatorHeight + 'px';
+			this.indicator.style.height = indicatorHeight + 'px';
+			
+			//Available height for the indicator to scroll divided by available height for the element to scroll
+			this.scrollbarToElementRatio = (wrapperHeight - indicatorHeight) / (scrollHeight - clientHeight);
+			
+			this.updateIndicatorPosition();
+		},
+		
+		updateIndicatorPosition: function () {
+			var scrollTop = this.scrollElement.scrollTop;
+			if (this.indicatorDeltaY != 0) {
+				this.scrollElement.scrollTop = scrollTop += this.indicatorDeltaY / this.scrollbarToElementRatio;
+				this.indicatorDeltaY = 0;
+			}
 					 
 			// Calculate the percentage that the element is currently scrolled and multiply it by the length the indicator can scroll
-			this.indicatorStyle.transform = 'translateY(' + this.scrollElement.scrollTop * this.scrollbarToElementRatio() + 'px)';
-		},
-
-
-		scrollbarToElementRatio: function () {
-			//Available height for the indicator to scroll divided by available height for the element to scroll
-			return (this.wrapperHeight - this.indicatorHeight) / (this.scrollElement.scrollHeight - this.scrollElement.clientHeight);
+			this.indicatorStyle.transform = 'translateY(' + scrollTop * this.scrollbarToElementRatio + 'px)';
 		},
 
 		start: function (e) {
@@ -182,8 +289,10 @@
 		move: function (e) {
 			var point = e.touches ? e.touches[0] : e;
 
-			this.offset += point.pageY - this.lastPointY;
+			this.indicatorDeltaY += point.pageY - this.lastPointY;
 			this.lastPointY = point.pageY;
+			
+			this.styledScroll.requestUpdate();
 
 			e.preventDefault();
 			e.stopPropagation();
